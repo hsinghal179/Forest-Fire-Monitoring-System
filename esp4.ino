111dc6e46ccafbfa90a551aeca6b71640ad253a0
Harshit Singhal <48367326+hsinghal179@users.noreply.github.com>
2021-05-04 21:36:34 +0530

    Add files via upload

---
+++ esp4.ino
@@ -0,0 +1,75 @@
+#include <SoftwareSerial.h>
+#define RX 2
+#define TX 3
+String AP = "Vartika";       // AP NAME
+String PASS = "tiggy@123"; // AP PASSWORD
+String API = "QACL8723EMOF8DG5";   // Write API KEY
+String HOST = "api.thingspeak.com";
+String PORT = "80";
+String field = "field1";
+int countTrueCommand;
+int countTimeCommand; 
+boolean found = false; 
+int valSensor = 1;
+SoftwareSerial esp8266(RX,TX); 
+ 
+  
+void setup() {
+  Serial.begin(9600);
+  esp8266.begin(115200);
+  sendCommand("AT",5,"OK");
+  sendCommand("AT+CWMODE=1",5,"OK");
+  sendCommand("AT+CWJAP=\""+ AP +"\",\""+ PASS +"\"",20,"OK");
+}
+
+void loop() {
+ valSensor = getSensorData();
+ String getData = "GET /update?api_key="+ API +"&"+ field +"="+String(valSensor);
+sendCommand("AT+CIPMUX=1",5,"OK");
+ sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+ sendCommand("AT+CIPSEND=0," +String(getData.length()+4),4,">");
+ esp8266.println(getData);delay(1500);countTrueCommand++;
+ sendCommand("AT+CIPCLOSE=0",5,"OK");
+}
+
+int getSensorData(){
+  float sensorValue;
+  sensorValue = analogRead(A0);
+  Serial.println(sensorValue);
+  return sensorValue;
+  
+}
+
+void sendCommand(String command, int maxTime, char readReplay[]) {
+  Serial.print(countTrueCommand);
+  Serial.print(". at command => ");
+  Serial.print(command);
+  Serial.print(" ");
+  while(countTimeCommand < (maxTime*1))
+  {
+    esp8266.println(command);//at+cipsend
+    if(esp8266.find(readReplay))//ok
+    {
+      found = true;
+      break;
+    }
+  
+    countTimeCommand++;
+  }
+  
+  if(found == true)
+  {
+    Serial.println("OYI");
+    countTrueCommand++;
+    countTimeCommand = 0;
+  }
+  
+  if(found == false)
+  {
+    Serial.println("Fail");
+    countTrueCommand = 0;
+    countTimeCommand = 0;
+  }
+  
+  found = false;
+ }
